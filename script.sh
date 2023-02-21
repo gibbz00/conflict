d5be6312f4ccdf45532c2906014ac64676a1eab0
gibbz00 <gabrielhansson00@gmail.com>
2023-02-22 00:02:39 +0100

    Implement pagination + bug fixing

--- script.sh
+++ script.sh
@@ -6,26 +6,44 @@ export total_prs_checked=0
 export conflicting_prs=()
 export skipped_prs=()
 
-# Get all open and non draft pull requests
-gh api -H "Accept: application/vnd.github+json" \
-  "repos/${OWNER}/${REPO}/pulls?per_page=100&state=open" --cache 1h \
-   --jq '.[] | select((.draft == false)) | .number | tostring + " "' |
+echo "Retrieving forks from ${OWNER}/${REPO}..."
+
+PR_NUMBERS=""
+RESPONSE=$(gh api --include -H "Accept: application/vnd.github+json" \
+  "repos/${OWNER}/${REPO}/pulls?per_page=100&state=open" --cache 1h)
+PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number | tostring | [.] | join(" ")' <<< "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
+LAST_PAGE_NR=$(rg --only-matching --pcre2 '\d*(?=>; rel="last")' <<< "$RESPONSE")
+   
+if test "$LAST_PAGE_NR" -gt "1"
+then
+  for PAGE in $(seq 2 "$LAST_PAGE_NR")
+  do
+    RESPONSE=$(gh api --include -H "Accept: application/vnd.github+json" \
+      "repos/${OWNER}/${REPO}/pulls?per_page=100&page=$PAGE&state=open" --cache 1h)
+    PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number | tostring | [.] | join(" ")' <<< "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
+  done
+fi
+
+echo "Found $(wc -l <<< "$PR_NUMBERS") open PRs"
+
+echo "$PR_NUMBERS" |
 xargs -I{} gh api -H "Accept: application/vnd.github+json" \
   "repos/${OWNER}/${REPO}/pulls/{}" --cache 1h \
    --jq 'select(.mergeable != false) | [.number, .head.sha] | join(" ")' |
 while read -r pr_num_n_sha
 do
   read -a arr <<< "$pr_num_n_sha"
-  echo "On PR ${arr[0]}..."
-  if test $(gh api -H "Accept: application/vnd.github+json" "repos/${OWNER}/${REPO}/commits/${arr[1]}/check-runs" --cache 1h --jq '.check_runs | [.[] | .conclusion] | all(. == "success")') = "true"
+  echo "Checking conflict with PR #${arr[0]}..."
+  if test "$(gh api -H "Accept: application/vnd.github+json" "repos/${OWNER}/${REPO}/commits/${arr[1]}/check-runs" --cache 1h --jq '.check_runs | [.[] | .conclusion] | all(. == "success")')" = "true"
   then
     git fetch --quiet origin "pull/${arr[0]}/head:TEMP_BRANCH_NAME" 1>/dev/null
-    if test $(git merge --no-commit --no-ff "TEMP_BRANCH_NAME" 2>&1 | rg --quiet "Automatic merge went well; stopped before committing as requested")
+    if test "$(git merge --no-commit --no-ff "TEMP_BRANCH_NAME" 2>&1 | rg "CONFLICT")"
     then   
       conflicting_prs+=("${arr[0]}")
     fi
-    git merge --abort 1>/dev/null
+    git merge --abort
     git branch --quiet --delete --force  "TEMP_BRANCH_NAME"
+    git prune
   else
     echo "PR is not passing checks, skipping..."
     skipped_prs+=("${arr[0]}")
