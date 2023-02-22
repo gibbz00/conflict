b5b3552007868cc7c81f3c72c8f91456af4268df
gibbz00 <gabrielhansson00@gmail.com>
2023-02-22 01:00:45 +0100

    minor formatting

--- script.sh
+++ script.sh
@@ -10,8 +10,9 @@ echo "Retrieving forks from ${OWNER}/${REPO}..."
 
 PR_NUMBERS=""
 RESPONSE=$(gh api --include -H "Accept: application/vnd.github+json" \
-  "repos/${OWNER}/${REPO}/pulls?per_page=100&state=open" --cache 1h)
-PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number | tostring | [.] | join(" ")' <<< "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
+  "repos/$OWNER/$REPO/pulls?per_page=100&state=open" --cache 1h)
+PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number' <<< \
+  "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
 LAST_PAGE_NR=$(rg --only-matching --pcre2 '\d*(?=>; rel="last")' <<< "$RESPONSE")
    
 if test "$LAST_PAGE_NR" -gt "1"
@@ -19,8 +20,9 @@ then
   for PAGE in $(seq 2 "$LAST_PAGE_NR")
   do
     RESPONSE=$(gh api --include -H "Accept: application/vnd.github+json" \
-      "repos/${OWNER}/${REPO}/pulls?per_page=100&page=$PAGE&state=open" --cache 1h)
-    PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number | tostring | [.] | join(" ")' <<< "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
+      "repos/$OWNER/$REPO/pulls?per_page=100&page=$PAGE&state=open" --cache 1h)
+    PR_NUMBERS="$(jq '.[] | select((.draft == false)) | .number' <<< \
+      "$(tail --lines=1 <<< "$RESPONSE")")$(printf "\n%s" "$PR_NUMBERS")"
   done
 fi
 
@@ -28,13 +30,14 @@ echo "Found $(wc -l <<< "$PR_NUMBERS") open PRs"
 
 echo "$PR_NUMBERS" |
 xargs -I{} gh api -H "Accept: application/vnd.github+json" \
-  "repos/${OWNER}/${REPO}/pulls/{}" --cache 1h \
+  "repos/$OWNER/$REPO/pulls/{}" --cache 1h \
    --jq 'select(.mergeable != false) | [.number, .head.sha] | join(" ")' |
 while read -r pr_num_n_sha
 do
   read -a arr <<< "$pr_num_n_sha"
   echo "Checking conflict with PR #${arr[0]}..."
-  if test "$(gh api -H "Accept: application/vnd.github+json" "repos/${OWNER}/${REPO}/commits/${arr[1]}/check-runs" --cache 1h --jq '.check_runs | [.[] | .conclusion] | all(. == "success")')" = "true"
+  if test "$(gh api -H "Accept: application/vnd.github+json" "repos/$OWNER/$REPO/commits/${arr[1]}/check-runs" --cache 1h \
+    --jq '.check_runs | [.[] | .conclusion] | all(. == "success")')" = "true"
   then
     git fetch --quiet origin "pull/${arr[0]}/head:TEMP_BRANCH_NAME" 1>/dev/null
     if test "$(git merge --no-commit --no-ff "TEMP_BRANCH_NAME" 2>&1 | rg "CONFLICT")"
