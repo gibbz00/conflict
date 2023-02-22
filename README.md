3efc834bcf8493feb6b95f199a6f998d7d566b72
gibbz00 <gabrielhansson00@gmail.com>
2023-02-22 01:46:35 +0100

    Report PRs that aren't mergeable with master

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-
 This script will:
 
 1) Gets all the open PRs from the specified repo. (Sorted by most recent first.)
 2) Filters out which are mergeable with origin master.
 3) Filter out those that pass all CI checks.
 4) Attempt to merge the PRs with local branch and report those that conflict.
+5) Summarize results.
 
 To try it out run:
 ```bash
--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ REPO="$2"
 
 export total_prs_checked=0
 export conflicting_prs=()
-export skipped_prs=()
+export unmergeable_with_master_prs=()
+export check_fail_prs=()
 
 echo "Retrieving forks from ${OWNER}/${REPO}..."
 
@@ -31,32 +32,41 @@ echo "Found $(wc -l <<< "$PR_NUMBERS") open PRs"
 echo "$PR_NUMBERS" |
 xargs -I{} gh api -H "Accept: application/vnd.github+json" \
   "repos/$OWNER/$REPO/pulls/{}" --cache 1h \
-   --jq 'select(.mergeable != false) | [.number, .head.sha] | join(" ")' |
-while read -r pr_num_n_sha
+   --jq '[.number, .head.sha, .mergeable] | join(" ")' |
+while read -r data
 do
-  read -a arr <<< "$pr_num_n_sha"
+  read -a arr <<< "$data"
   echo "Checking conflict with PR #${arr[0]}..."
-  if test "$(gh api -H "Accept: application/vnd.github+json" "repos/$OWNER/$REPO/commits/${arr[1]}/check-runs" --cache 1h \
-    --jq '.check_runs | [.[] | .conclusion] | all(. == "success")')" = "true"
-  then
-    git fetch --quiet origin "pull/${arr[0]}/head:TEMP_BRANCH_NAME" 1>/dev/null
-    if test "$(git merge --no-commit --no-ff "TEMP_BRANCH_NAME" 2>&1 | rg "CONFLICT")"
-    then   
-      conflicting_prs+=("${arr[0]}")
-    fi
-    git merge --abort
-    git branch --quiet --delete --force  "TEMP_BRANCH_NAME"
-    git prune
+
+  if test "${arr[2]}" = "false"
+  then 
+    echo "PR is not mergeable with origin master, skipping..."
+    unmergeable_with_master_prs+=("${arr[0]}")
   else
-    echo "PR is not passing checks, skipping..."
-    skipped_prs+=("${arr[0]}")
+    if test "$(gh api -H "Accept: application/vnd.github+json" "repos/$OWNER/$REPO/commits/${arr[1]}/check-runs" --cache 1h \
+      --jq '.check_runs | [.[] | .conclusion] | all(. == "success")')" = "true"
+    then
+      git fetch --quiet origin "pull/${arr[0]}/head:TEMP_BRANCH_NAME" 1>/dev/null
+      if test "$(git merge --no-commit --no-ff "TEMP_BRANCH_NAME" 2>&1 | rg "CONFLICT")"
+      then   
+        conflicting_prs+=("${arr[0]}")
+      fi
+      git merge --abort
+      git branch --quiet --delete --force  "TEMP_BRANCH_NAME"
+      git prune
+    else
+      echo "PR is not passing checks, skipping..."
+      check_fail_prs+=("${arr[0]}")
+    fi
   fi
+  
   total_prs_checked=$((total_prs_checked + 1))
 
   echo "== SUMMARY =="
   echo "Number of PRs checked $total_prs_checked"
   echo "Nuber of PRs with conflicts ${#conflicting_prs[*]}"
   echo "Conflicting PRs: " "${conflicting_prs[@]}"
-  echo "Skipped PRs: " "${skipped_prs[@]}"
+  echo "Checks faiure PRs: " "${check_fail_prs[@]}"
+  echo "Unmergeable with master PRs: " "${unmergeable_with_master_prs[@]}"
   echo ""
 done
